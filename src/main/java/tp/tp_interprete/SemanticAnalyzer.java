5d066a6cb95f3e78331a4a1ea239e730e7c679ed
luciano <lucianorodriguez1@users.noreply.github.com>
2026-06-20 22:11:59 -0300

    agregar metodos visit de declarar y asignar

--- src/main/java/tp/tp_interprete/SemanticAnalyzer.java
+++ src/main/java/tp/tp_interprete/SemanticAnalyzer.java
@@ -1,6 +1,31 @@
 package tp.tp_interprete;
 
+import tp.tp_interprete.MiniLangParser.*;
 
-public class SemanticAnalyzer extends MiniLangBaseVisitor<Object>{
+public class SemanticAnalyzer extends MiniLangBaseVisitor<Object> {
+    
+    private SymbolTable symbolTable = new SymbolTable();
 
+    @Override
+    public Object visitDeclaration(DeclarationContext ctx) {
+        String type = ctx.TYPE().getText(); // "int", "float", etc.
+        String id = ctx.ID().getText();     // nombre de la variable
+        
+        symbolTable.declaration(id, type);
+        
+        return null; 
+    }
+
+
+    @Override
+    public Object visitAssignment(AssignmentContext ctx) {
+        String id = ctx.ID().getText();
+        
+        Object value = visit(ctx.expression());
+        
+        // llamas al metodo de asignación
+        symbolTable.assign(id, value);
+        
+        return null;
+    }
 }
